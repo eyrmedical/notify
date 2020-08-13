496e28e303fef2594088f1c210f0d1b2ecb54f61
Vyacheslav Voronchuk <voronchuk@gmail.com>
2020-08-13 17:44:21 +0300

    Removed unused tests

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Notify.Mixfile do
 
   def project do
     [app: :notify,
-     version: "0.1.11",
+     version: "0.1.12",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
--- test/notify_test.exs
+++ test/notify_test.exs
@@ -1,28 +1,20 @@
 defmodule NotifyTest do
 	use ExUnit.Case
+	alias Notify.Notification
 	doctest Notify
 
-	@test_apn true
-	@test_fcm true
-	@test_gcm false
-
-	@ios {:ios, Application.get_env(:notify, :device_token_ios) }
-	@android {:android, Application.get_env(:notify, :device_token_android) }
 	@voip %Notification{
 		sound: "eyr.m4a",
 		data: %{
-			"voip": true,
-			"name": "Notify Test",
-			"provider": 1,
-			"key": "45589032",
-			"session_id": "1_MX40NTU4OTAzMn5-MTQ5NjY3MjkwNjg0OH5sUDhTSGU0Z3UrK0gyOG5LcUNZYlJhS3p-UH4",
-			"token": "T1==cGFydG5lcl9pZD00NTU4OTAzMiZzaWc9MDQ2Y2UzNDA4ZjFmYTA5ODIzNzMyNDc5OGI1OWIzYWU1NTIxMjg2ZjpzZXNzaW9uX2lkPTFfTVg0ME5UVTRPVEF6TW41LU1UUTVOalkzTWprd05qZzBPSDVzVURoVFNHVTBaM1VySzBneU9HNUxjVU5aWWxKaFMzcC1VSDQmY3JlYXRlX3RpbWU9MTQ5NjY3MjkwNyZub25jZT0wLjk4Mzk1NjQxMTAwOTU0NzYmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTQ5Njc1OTMwNw=="
+			"voip" => true,
+			"name" => "Notify Test",
+			"provider" => 1,
+			"key" => "45589032",
+			"session_id" => "1_MX40NTU4OTAzMn5-MTQ5NjY3MjkwNjg0OH5sUDhTSGU0Z3UrK0gyOG5LcUNZYlJhS3p-UH4",
+			"token" => "T1==cGFydG5lcl9pZD00NTU4OTAzMiZzaWc9MDQ2Y2UzNDA4ZjFmYTA5ODIzNzMyNDc5OGI1OWIzYWU1NTIxMjg2ZjpzZXNzaW9uX2lkPTFfTVg0ME5UVTRPVEF6TW41LU1UUTVOalkzTWprd05qZzBPSDVzVURoVFNHVTBaM1VySzBneU9HNUxjVU5aWWxKaFMzcC1VSDQmY3JlYXRlX3RpbWU9MTQ5NjY3MjkwNyZub25jZT0wLjk4Mzk1NjQxMTAwOTU0NzYmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTQ5Njc1OTMwNw=="
 		},
+		token: "T1==cGFydG5lcl9pZD00NTU4OTAzMiZzaWc9MDQ2Y2UzNDA4ZjFmYTA5ODIzNzMyNDc5OGI1OWIzYWU1NTIxMjg2ZjpzZXNzaW9uX2lkPTFfTVg0ME5UVTRPVEF6TW41LU1UUTVOalkzTWprd05qZzBPSDVzVURoVFNHVTBaM1VySzBneU9HNUxjVU5aWWxKaFMzcC1VSDQmY3JlYXRlX3RpbWU9MTQ5NjY3MjkwNyZub25jZT0wLjk4Mzk1NjQxMTAwOTU0NzYmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTQ5Njc1OTMwNw==",
 		voip: true
-	}
-	@regular %Notification{
-		title: "NotifyTest regular notification",
-		message: "This is a message from the Notify package written in Elixir",
 	}
 
 	setup_all _context do
@@ -42,32 +34,4 @@ defmodule NotifyTest do
 		assert intial_token != new_token
 		assert String.length(new_token) == 200
 	end
-
-	if @test_apn do
-		test "iOS push notification" do
-			assert {:ok, _id} = Notify.push(@ios, @regular)
-		end
-
-		test "iOS push notification to invalid device_id fails" do
-			assert {:error, "BadDeviceToken"} = Notify.push({:ios, "WRONG"}, @regular)
-		end
-
-
-		test "iOS VOIP notification" do
-			assert {:ok, _id} = Notify.push(@ios, @voip)
-		end
-	end
-
-	if @test_fcm do
-		test "Android VOIP notification" do
-			assert {:ok, _id} = Notify.push(@android, @voip)
-		end
-	end
-
-	if @test_gcm do
-		test "Android VOIP notification" do
-			assert {:ok, _id} = Notify.push(@android, @voip)
-		end
-	end
-
 end
