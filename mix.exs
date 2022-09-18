e16e2d2b188ae25e95b6cc1743615a8b1243528e
Mikal Villa <m@sigterm.no>
2022-09-19 00:53:54 +0200

    Hopefully the fix for development notifications

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Notify.Mixfile do
   def project do
     [
       app: :notify,
-      version: "0.4.4",
+      version: "0.4.5",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
